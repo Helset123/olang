5598b0704d6b81653ac5ef5787145e054e01dbe8
Otto Roming <otto.roming@protonmail.com>
2025-01-11 22:07:47 +0100

    removed redundant tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -2,9 +2,7 @@ use super::*;
 
 #[test]
 fn fibonacci_sequence() {
-    assert_eq!(eval(include_str!("fib20.olang")).unwrap(), Value::Int(6765));
-    assert_eq!(eval(include_str!("fib10.olang")).unwrap(), Value::Int(55));
-    assert_eq!(eval(include_str!("fib3.olang")).unwrap(), Value::Int(2));
+    assert_eq!(eval(include_str!("fib.olang")).unwrap(), Value::Int(6765));
 }
 
 #[test]
