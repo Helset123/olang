808dbf0a9a9cb8ee2081a94ac987fc5bb4e9167a
Otto Roming <otto.roming@protonmail.com>
2025-01-16 23:17:04 +0100

    added a working cli interface using the structopt crate

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use ::std::fs::File;
 use anyhow::Result;
 use interpreter::EvalError;
+use std::{io::Read, path::PathBuf};
+use structopt::StructOpt;
 use value::Value;
-
-use std::fs;
 
 mod builtin;
 mod environment;
@@ -14,14 +15,45 @@ mod value;
 #[cfg(test)]
 mod tests;
 
+/// The easy to use interpreter
+#[derive(StructOpt, Debug)]
+#[structopt(name = "olang")]
+struct Options {
+    // /// print token sequence and AST of the source code
+    // #[structopt(short, long)]
+    // debug: bool,
+    /// Source string to process
+    #[structopt(short, long)]
+    command_string: Option<String>,
+
+    /// Source file to process
+    #[structopt(name = "FILE", parse(from_os_str))]
+    file: Option<PathBuf>,
+}
+
 pub fn eval(source: &str) -> Result<Value, EvalError> {
     interpreter::Interpreter::new().eval(source)
 }
 
 fn main() -> Result<()> {
-    let source = fs::read_to_string("source.olang")?;
+    run_cli()?;
+    Ok(())
+}
 
-    println!("tokens: {:#?}", lexer::Lexer::new(&source).tokenize()?);
-    eval(source.as_str())?;
+pub fn run_cli() -> Result<()> {
+    let options = Options::from_args();
+
+    if let Some(command) = options.command_string {
+        eval(command.as_str())?;
+    };
+
+    if let Some(path) = options.file {
+        let mut file = File::open(path)?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+
+        eval(content.as_str())?;
+    };
+
     Ok(())
 }
